058383445955a75d9f50cd3a53a2383801166b89
Junior Fontenele <github@juniorfontenele.com.br>
2025-04-19 20:39:59 -0300

    fix: Set default client_id in vault configuration to ensure proper initialization

--- config/vault.php
+++ config/vault.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 return [
     'url' => env('VAULT_URL', config('app.url') . '/vault'),
     'issuer' => env('VAULT_ISSUER', config('app.url')),
-    'client_id' => env('VAULT_CLIENT_ID', null),
+    'client_id' => env('VAULT_CLIENT_ID', 'vault-client-id'),
 
     'token_expiration_time' => 60, // 1 minute
     'cache_ttl' => 60 * 60, // 1 hour
