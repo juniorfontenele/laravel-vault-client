77704de82a3d85faf0b81042022ea29b4e900951
Junior Fontenele <github@juniorfontenele.com.br>
2025-04-20 21:12:14 -0300

    Add provisioning_token config

--- config/vault.php
+++ config/vault.php
@@ -5,7 +5,8 @@ declare(strict_types = 1);
 return [
     'url' => env('VAULT_URL', config('app.url') . '/vault'),
     'issuer' => env('VAULT_ISSUER', config('app.url')),
-    'client_id' => env('VAULT_CLIENT_ID', 'vault-client-id'),
+    'client_id' => env('VAULT_CLIENT_ID'),
+    'provisioning_token' => env('VAULT_PROVISIONING_TOKEN'),
 
     'token_expiration_time' => 60, // 1 minute
     'cache_ttl' => 60 * 60, // 1 hour
